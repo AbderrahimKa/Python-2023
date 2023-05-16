6d9490c2a367b8abdeae870018c3c80e4a556a38
AbderrahimKa <abderrahimkaabachi@gmail.com>
2023-05-16 13:27:48 +0100

    first commit

---
+++ Week 1/Day 1/Optional/for_loop_basic1.py
@@ -0,0 +1,16 @@
+print('Basic')
+for i in range(151):
+    print(i)
+
+print('Multiples of Five')
+i = 5
+while i<1001 :
+    print(i)
+    i+=5
+
+print('Counting, the Dojo Way')
+for i in range(1,100):
+    if(i % 5 == 0):
+        print('Coding')
+    if(i % 10 == 0):
+        print('Coding Dojo')
