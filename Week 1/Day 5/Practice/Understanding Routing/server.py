eb7014c567df35ab5c67a5648eb650165abf7c78
AbderrahimKa <abderrahimkaabachi@gmail.com>
2023-05-19 13:15:13 +0100

    understood the routes

---
+++ Week 1/Day 5/Practice/Understanding Routing/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def hello_dojo():
+    return 'Dojo!'
+
+@app.route('/say/<whatever>')
+def function1(whatever):
+    return 'Hi ' + whatever
+
+@app.route('/repeat/<int:times>/<word>')
+def repeat(times,word):
+    return (word + ' ') * times
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
