79d94a2d139263a528824b1af52a6c71430d07e2
AbderrahimKa <abderrahimkaabachi@gmail.com>
2023-05-26 15:48:43 +0100

    html table commit

--- Week 1/Day 5/Practice/CheckBoard/server.py
+++ Week 1/Day 5/Practice/CheckBoard/server.py
@@ -1,13 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+app.route('/')
 def index():
-    return render_template('index.html')
-
-# @app.route('/')
-# def hello():
-#     return render_template('index.html')
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
