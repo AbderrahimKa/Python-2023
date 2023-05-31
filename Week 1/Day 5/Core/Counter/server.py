5bc180f0bb6a223afb4991776d8b0fcd11bf7b4b
AbderrahimKa <abderrahimkaabachi@gmail.com>
2023-05-31 16:29:38 +0100

    Users CRUD Modularized

--- Week 1/Day 5/Core/Counter/server.py
+++ Week 1/Day 5/Core/Counter/server.py
@@ -12,17 +12,15 @@ def home():
         session['counter'] = 0
     return render_template('index.html', counter=session['counter'])
 
+@app.route('/increment', methods=['POST'])
+def increment():
+    session['counter'] += 2
+    return redirect(url_for('home'))
 
 @app.route('/destroy_session')
 def destroy_session():
     session.clear()
     return redirect('/')
-
-
-@app.route('/increment', methods=['POST'])
-def increment():
-    session['counter'] += 2
-    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
